f7b03d5f04218a6d1e39b7af135ea86f9903486c
mjfy93 <maria.jose.ferro@hotmail.com>
2025-08-08 01:00:18 +0000

    Excuse Generator finished

--- src/app.js
+++ src/app.js
@@ -1,27 +1,22 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["My dog", "A thief", "My grandfather", "I", "My sister"];
+let action = ["destroyed", "broke", "lost", "fell off", "jumped off"];
+let what = ["our roof", "our house", "my dad's car", "my bike", "my laptop"];
+let when = ["today.", "last night.", "a few hours ago.", "yesterday.", "this weekend."];
 
-window.onload = function() {
-    // Call your excuse-generating function here
-  const excuse = generateExcuse(); // Assuming your function is named generateExcuse
-  document.getElementById('excuse').innerHTML = excuse
-}
- function generateExcuse(){
-  let who = ["My dog", "A thief", "My grandfather", "I", "My sister"];
-  let action = ["destroyed", "broke", "lost", "fell off", "lost"];
-  let what = ["our roof", "our house", "my dad's car", "my bike", "my laptop"];
-  let when = ["today.", "last night.", "a few hours ago.", "yesterday.", "this weekend."];
-  
-  let randomWho= who[Math.floor (Math.random()*who.length)];
-  let randomAction= action[Math.floor (Math.random()*action.length)];
-  let randomWhat= what[Math.floor(Math.random()*what.length)];
-  let randomWhen= when[Math.floor (Math.random()*when.length)];
 
-let excuse = "I'm so sorry!" + " " + `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
-return excuse;
-}
-console.log (generateExcuse);
+let randomWho = who[Math.floor(Math.random() * who.length)];
+let randomAction = action[Math.floor(Math.random() * action.length)];
+let randomWhat = what[Math.floor(Math.random() * what.length)];
+let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+window.onload = function () {
+  document.querySelector('#excuse').innerHTML = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`
+};
+
+
+
+
+
+
